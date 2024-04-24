9a4040c129e863d57e160f6a9a3f3e369394cb31
ian.mendonca <ian.mendonca@polodeinovacao.ifpb.edu.br>
2024-04-24 19:44:12 -0300

    First Commit

---
+++ src/main/java/br/edu/ifpb/pweb2/armants/ArmAntsPweb2Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.armants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmAntsPweb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArmAntsPweb2Application.class, args);
+    }
+
+}
